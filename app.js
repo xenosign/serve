577ce98080a422cf6f225c2f1eeb6b5a40a8fd0e
xenosign <xenosign@naver.com>
2023-05-08 13:27:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, "/serve/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/serve/build/index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 app.listen(app.get("port"), () => {
